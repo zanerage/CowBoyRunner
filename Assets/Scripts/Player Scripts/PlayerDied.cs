60bb5650006879389adc30098e5597980f08ca91
Mario Antolovic <marioantolovic0@gmail.com>
2020-01-11 21:21:35 +0100

    played died / End Game

---
+++ Assets/Scripts/Player Scripts/PlayerDied.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDied : MonoBehaviour
+{
+    public delegate void EndGame();
+    public static event EndGame endgame;
+
+    void PlayerDiedEndGame()
+    {
+        if (endgame != null)
+            endgame();
+
+        Destroy(gameObject);
+    }
+
+     void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag=="Collector")
+        {
+            PlayerDiedEndGame();
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Zombie")
+        {
+            PlayerDiedEndGame();
+        }
+    }
+  
+
+  
+}
